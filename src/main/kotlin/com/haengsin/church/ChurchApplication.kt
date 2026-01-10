85901f181156d841a92db73d4a8ad0e145b451ac
dante-delivered <dante@delivered.co.kr>
2026-01-11 00:03:28 +0900

    feature: 프로젝트 init / local db 구축

---
+++ src/main/kotlin/com/haengsin/church/ChurchApplication.kt
@@ -0,0 +1,11 @@
+package com.haengsin.church
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChurchApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChurchApplication>(*args)
+}
---
+++ src/test/kotlin/com/haengsin/church/ChurchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.haengsin.church
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChurchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
