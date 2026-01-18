cfdc3d7b0a03a99525248a43745f31bd4d6eb7b9
dante-delivered <dante@delivered.co.kr>
2026-01-18 18:52:32 +0900

    feature: cors 허용 추가

---
+++ src/main/kotlin/com/haengsin/church/configuration/CorsConfiguration.kt
@@ -0,0 +1,19 @@
+package com.haengsin.church.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfiguration : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .exposedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
